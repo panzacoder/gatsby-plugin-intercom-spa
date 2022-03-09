1a20a4f35e3814900c64e938422e91b2409b85a4
Jake Hebert <jacobshebert@gmail.com>
2022-03-09 16:10:15 -0600

    Putting the script back into one line

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -19,7 +19,7 @@ exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
           window.HideDefaultLauncher = ${hideDefaultLauncher};
           window.CustomLauncherSelector = ${
             pluginOptions.custom_launcher_selector || ""
-          };
+          }
           (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/${
             pluginOptions.app_id
           }';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}function ld(){setTimeout(l, ${
